bf169108e3e8a1adfd9966d9224fcc606933fbf7
Samson Awe <awesamson08@gmail.com>
2025-09-02 20:44:20 +0100

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '80px', backgroundColor: 'gray' }}>
+        <p style={{ color: 'white', fontSize: '14px' }}>© 2023 City Lovers</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}>
+        <p style={{ color: 'blue', fontWeight: '700', fontSize: '1.3rem' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default MainContent

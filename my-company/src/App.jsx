f28a3d1df54af159aa38663359ce7d2df1cbfb22
Samson Awe <awesamson08@gmail.com>
2025-09-03 16:26:15 +0100

    Moved Home, About, Services and Contact to components

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Services from './Services'
-import Contact from './Contact'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
 import Navbar from './components/Navbar'
-import Footer from './Footer'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {

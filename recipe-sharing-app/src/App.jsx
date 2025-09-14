5702b88b1bcdc389c148eedd661d5e497285a2b9
Samson Awe <awesamson08@gmail.com>
2025-09-14 22:43:53 +0100

    Add Router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
@@ -8,14 +8,16 @@ import './App.css'
 function App() {
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
-      </Routes>
-    </div>
+    <Router>
+      <div style={{ padding: '2rem' }}>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+          <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 

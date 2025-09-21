89b4851ab28a4ce5ac0b465d5c7170cc68cfe4c4
Samson Awe <awesamson08@gmail.com>
2025-09-21 18:55:03 +0100

    Setting Up a React Project for GitHub User Search Application

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <div style={{ padding: '2rem' }}>
         <h1>Recipe Sharing App</h1>
         <Routes>
@@ -30,7 +30,7 @@ function App() {
           <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
         </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
